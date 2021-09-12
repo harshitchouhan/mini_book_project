4b97954372493dbd8f813c89a70c67c679112009
Harshit <harshitsinghchouhan2018@gmail.com>
2021-09-12 16:37:45 +0530

    book component created

--- src/index.js
+++ src/index.js
@@ -1,7 +1,31 @@
-import ReactDom from "react-dom";
+import ReactDom from 'react-dom';
 
-function Greetings() {
-  return <h4>Mini Book Project</h4>;
+function BookList() {
+  return (
+    <section>
+      <Book></Book>
+    </section>
+  );
 }
 
-ReactDom.render(<Greetings></Greetings>, document.getElementById("root"));
+const Book = () => {
+  return (
+    <article>
+      <Image></Image>
+      <Title></Title>
+      <Author></Author>
+    </article>
+  );
+};
+
+const Image = () => (
+  <img
+    src='https://images-eu.ssl-images-amazon.com/images/I/81dQwQlmAXL._AC_UL200_SR200,200_.jpg'
+    alt='Book'
+  />
+);
+
+const Title = () => <h1>Rich Dad Poor Dad: What the Rich Teach...</h1>;
+const Author = () => <h4>Robert T. Kiyosaki</h4>;
+
+ReactDom.render(<BookList></BookList>, document.getElementById('root'));
